084559d397347e7159d964272cd642ec30b8b7aa
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-16 03:05:49 +0000

    [Add]public items controllerのindexに定義を追加、showアクションを定義、privateを追記

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,7 +2,18 @@ class Public::ItemsController < ApplicationController
     
     def index
         @items = Item.all
+        @item = Item.find(params[:id])
     end
+    
+    def show
+        @item = Item.find(params[:id])
+    end 
+    
+    private
+    
+    def item_params
+        params.require(:item).permit(:name, :introduction, :image, :price)
+    end 
     
     
 end
