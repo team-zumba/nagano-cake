bda2d84b9310113c1130e593b8ccea52e65d2167
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-16 10:12:01 +0000

    [Add]管理者ユーザーのログイン、ログアウト後の画面遷移先を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+  
+  private
+  
+  # ログイン後の画面遷移先設定
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      # adminユーザー
+      admin_orders_path
+      
+    else
+      # customerユーザー
+      root_path
+    end
+  end
+  
+  # ログアウト後の画面遷移先設定
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      # adminユーザー
+      new_admin_session_path
+      
+    else
+      # customerユーザー
+      root_path
+    end
+  end
 end
