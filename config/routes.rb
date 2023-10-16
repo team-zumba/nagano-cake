98e74f4b32f89889b4d240026a1f3e50151fc47d
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-16 05:55:08 +0000

    [Add]publicとadminのrouteを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,33 @@
 Rails.application.routes.draw do
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions: 'public/sessions',
   }
   
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :public do
+    resources :addresses
+    resources :cart_items
+    resources :confirmation
+    resources :customers
+    resources :homes
+    resources :items
+    resources :orders
+    resources :application
+  end
+  
+  namspace :admin do
+    resources :customers
+    resources :genres
+    resources :homes
+    resources :items
+    resources :order_details
+    resources :orders
+  end
+  
   # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
