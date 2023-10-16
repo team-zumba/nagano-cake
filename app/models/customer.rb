122a1da4fd689d4d914ccd64fb788806a8e068bc
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-10-16 12:15:00 +0000

    [Add]バリデーション実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  validates :last_name_kana, :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }       
+  validates :last_name_kana, :first_name_kana,　presence: true       
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。' }  , presence: true 
+  validates :email, uniqueness: true, presence: true
+  validates :telephone_number, uniqueness: true, presence: true
+  validates :postal_code, uniqueness: true, presence: true
+  validates :address, uniqueness: true, presence: true
+  
 end
