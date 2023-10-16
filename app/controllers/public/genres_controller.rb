f256ff6a38f4404e88f9499c90ec8820827f16ff
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-16 15:29:33 +0000

    [Feat]genreコントローラーのfilterアクションを非同期で実装

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -1,2 +1,12 @@
 class Public::GenresController < ApplicationController
+  def fliter
+    if params[:id] == '0'
+      # 全商品のジャンルが選ばれた場合
+      @items = Item.all
+      
+    else
+      # その他のジャンルが選ばれた場合
+      @items = Item.where(genre_id: params[:id])
+    end
+  end
 end
