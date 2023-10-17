eb197ebd6811da53f21c195fd392f0ec1fefc61f
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-17 14:18:31 +0000

    [Add]admin items controllerの定義を追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,30 @@
 class Admin::ItemsController < ApplicationController
+    
+    def new
+        @item = Item.new(item_params)
+    end 
+    
+    def create
+        @item = Item.new(item_params)
+        @item.save
+    end 
+    
+    def index
+        @items = Item.all
+    end 
+    
+    def show
+        @item = Item.find(params[:id])
+    end 
+    
+    def edit
+        @item = Item.find(params[:id])
+    end 
+    
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to edit_admin_item_path(@item.id)
+    end 
+    
 end
