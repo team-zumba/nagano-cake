e62b4ec119a18c9b1ea15130fbe3c66a5d78d7b4
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-17 12:32:25 +0000

    [Add]customers controllerのleave定義を追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,12 +11,20 @@ class Public::CustomersController < ApplicationController
     end 
     
     def update
-        @customer = Customer.find(params[:id])
+        @customer = current_customer
         @customer.update(customer_params)
         redirect_to edit_customer_path(@customer.id)
     end 
     
     def check
+        # @customer = current_customer
+        # @leave = @customer.is_active == false
+    end 
+    
+    def leave
+        current_customer.update(is_active: false)
+        # flash[:notice] = "退会しました。"
+        redirect_to root_path, notice: "退会しました。"
     end 
     
     private
@@ -24,5 +32,7 @@ class Public::CustomersController < ApplicationController
     def customer_params
         params.require(:customer).permit(:email, :last_name, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
     end 
+    
+    
 
 end
