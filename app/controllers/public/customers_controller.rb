cec892aa056b8fedec05c465b322cce06225e7b7
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-17 01:29:17 +0000

    [Feature]customers controllerに定義を記述

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,20 @@
 class Public::CustomersController < ApplicationController
+    
+    def show
+        @customer = Customer.find(params[:id])
+        @addresses = @customer.addresses.all
+        @orders = @customer.orders.all
+    end 
+    
+    def edit 
+        @customer = Customer.find(params[:id])
+    end 
+    
+    def update
+        @customer = Customer.find(params[:id])
+        @customer.update(customer_params)
+        redirect_to edit_public_customer(@customer.id)
+    end 
+    
+
 end
