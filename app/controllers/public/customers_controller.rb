9b172f854dfe7ef8707ad80ffedb8e9c35002e3c
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-17 05:17:34 +0000

    [Add]Customers controllerにparamsとconfirmationの定義を追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -16,5 +16,13 @@ class Public::CustomersController < ApplicationController
         redirect_to edit_public_customer(@customer.id)
     end 
     
+    def confirmation
+    end 
+    
+    private
+    
+    def customer_params
+        params.require(:customer).permit(:email, :last_name, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
+    end 
 
 end
