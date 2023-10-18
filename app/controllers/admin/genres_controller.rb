7d67f0b4a47c1693de976a8e2f6032a3ab7d80b1
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-18 18:30:52 +0000

    [Feat]ジャンルの新規登録機能を実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,4 +5,20 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.new
     @genres = Genre.all
   end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to admin_genres_path, notice: "#{genre.name}のジャンルを追加しました。"
+    else
+      @genres = Genre.all
+      render :index
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
