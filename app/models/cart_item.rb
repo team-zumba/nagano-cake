e53c612d0aedd0383f1c4ab60d0ed100ffce6d6d
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-18 10:31:23 +0000

    [Add]CartItemモデルにメソッドとアソシエーションを設定

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,13 @@
 class CartItem < ApplicationRecord
+  belongs_to :item
+
+  # 小計を求めるメソッド
+  def subtotal
+    item.item_price * amount
+  end
+
+  # 在庫数から0までの配列を返す
+  def get_amount_array
+    [*0..amount]
+  end
 end
