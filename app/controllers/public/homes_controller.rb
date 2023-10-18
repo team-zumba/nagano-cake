23b19960fce8c6fefba241cc46c123c327dff80e
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-18 15:41:08 +0000

    [Fix]論理削除された商品をメインビジュアル商品にピックアップさせない

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,11 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
+    
     # メインビジュアル用の商品をランダムで3つ取得
-    @rand_items = Item.all.sample(5)
+    @rand_items = Item.where(is_active: true)
+    @rand_items.sample(5)
+
     # 新規作成順に並び替えて、10件のItemモデルを取得
     @new_items = Item.order(created_at: :desc).limit(10)
   end
