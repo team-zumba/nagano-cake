50cd9fa698c276689e20c0f3d1bbfe5e8f6c4055
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-18 15:25:34 +0000

    [Fix]adminユーザーログイン後の遷移先パス名を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
       # adminユーザー
-      admin_orders_path
+      admin_root_path
       
     else
       # customerユーザー
