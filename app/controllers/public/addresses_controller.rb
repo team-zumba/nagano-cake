8cfe3c2d03dbe16e5aaa371a950bbccbafade849
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-10-21 11:14:48 +0000

    [Add]配送先登録フォーム作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,3 +1,20 @@
 class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
+  
+  def index
+    @address = Address.new
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
 end
