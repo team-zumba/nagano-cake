e55cc8c891a506e42fa33b418f7265df06c54cf0
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-21 10:39:23 +0000

    [Add]orderモデルのvalidationを追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,15 @@ class Order < ApplicationRecord
   has_many :order_details
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { unpaid: 0, payment_confirmed: 1, making: 2, shipping_in_process: 3, shipping_complete: 4 }
-  has_many :order_details
-  belongs_to :customer
   
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :postal_code, presence: true
+  validates :total_payment, presence: true
+  validates :statsus, presence: true
+  validates :payment_method, presence: true
+
+
    def item_price
     (price * 1.1).floor
    end
