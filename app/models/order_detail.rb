b040b3c747a4d41cc3cc3523f179f17407c3f9f9
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-21 07:49:16 +0000

    [Add]アソシエーションを追加

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,9 +1,4 @@
 class OrderDetail < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :order
   belongs_to :item
-=======
-    belongs_to :order
-    belongs_to :item
->>>>>>> 21d73f9 ([Add]アソシエーションを追加)
 end
