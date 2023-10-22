7f2cbb753a36f659da03410eb6055e2493a7ed1a
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-10-22 12:52:16 +0000

    [Add]paginationを追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,7 +19,7 @@ class Admin::ItemsController < ApplicationController
     end 
     
     def index
-        @items = Item.all
+        @items = Item.page(params[:page])
     end 
     
     def show
