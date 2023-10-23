eb10a635657e324d202f52010e981fbafe944cb9
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-23 10:39:47 +0000

    [Add]商品新規投稿ページのエラー処理を実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,8 +14,19 @@ class Admin::ItemsController < ApplicationController
     
     def create
         @item = Item.new(item_params)
-        @item.save
-        redirect_to admin_item_path(@item.id)
+        if @item.save
+            redirect_to admin_item_path(@item.id)
+        else
+            @genres = Genre.all
+            select_genres = @genres.pluck(:name)
+            
+            @select_array = []
+            select_genres.each_with_index do |genre_name, i|
+                genre_array = [genre_name, i + 1]
+                @select_array.push(genre_array)
+            end 
+            render :new
+        end
     end 
     
     def index
