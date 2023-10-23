39530580e0bcc5b3f47a0cf4b48fa7b7d12584c5
yusuke-sudou <143806854+yusuke-sudou@users.noreply.github.com>
2023-10-23 05:39:59 +0000

    [Add]製作ステータス作成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,10 @@
 class Admin::OrderDetailsController < ApplicationController
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_to admin_order_path(@order.id)
+  end
+  
 end
