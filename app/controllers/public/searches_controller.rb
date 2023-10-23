7d3eabaedf2e3a774bf4f81eabcf9fa08dc1b12f
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-23 05:56:59 +0000

    [Feat]商品検索機能を実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,2 +1,7 @@
 class Public::SearchesController < ApplicationController
+  def items_search
+    @word = params[:word]
+    @items = Item.where('name LIKE?', '%' + params[:word] + '%')
+    @genres = Genre.all
+  end
 end
